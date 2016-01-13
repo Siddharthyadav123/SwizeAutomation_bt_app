3dd2edb82b0cdfb07542d9c721739f079acdc8c9
Siddharth <siddharthyadav123>
2016-01-13 18:40:09 +0530

    added dummy code

---
+++ SvizeAutomation/app/src/main/java/com/svizeautomation/app/screens/EditRoomActivity.java
@@ -0,0 +1,15 @@
+package com.svizeautomation.app.screens;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by siddharth on 1/13/2016.
+ */
+public class EditRoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
